9ca0c1893c5d56f4ea222022f0a852ddf051c1f6
haristhedeveloper <152081635+haristhedeveloper@users.noreply.github.com>
2024-08-16 23:06:58 -0700

    Update Chatbot2.py

--- Chatbot2.py
+++ Chatbot2.py
@@ -1,17 +1,16 @@
-import streamlit as st
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Access the API key
-groq_api_key = os.getenv("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")
+groq_api_key = os.getenv("api")
 
 # Now you can use the API key in your requests
 from groq import Groq
 
 # Initialize the Groq client with the API key
-client = Groq(api_key="groq_api_key")
+client = Groq(api_key=groq_api_key)
 
 print("Hello! I am expert in programming fell free to ask or type 'bye' to exit")
 # Loop to continuously ask the user for input
